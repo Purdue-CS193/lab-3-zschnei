81a9dfb61a9aa618dac0f18bd9643bfb7634f1c8
Zach Schneider <zschnei@purdue.edu>
2018-09-27 12:53:20 -0400

    Changed name to mine

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -3,10 +3,10 @@ public class BuggyUtilClient {
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
-        System.out.println("In: first = " + "\"Sean\"" +
-                ", last = " + "\"Flannery\"");
+        System.out.println("In: first = " + "\"Zach\"" +
+                ", last = " + "\"Schneider\"");
         System.out.println("Out: " +
-        StringUtils.makePurdueUsername("Sean", "Flannery"));
+        StringUtils.makePurdueUsername("Zach", "Schneider"));
     }
 }
 
